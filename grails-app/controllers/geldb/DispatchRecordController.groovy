fc4b66a6890c1c59d7c38d58f94850b3348e3386
Nasullah Alham <nasullah.khalidalham@ndcls.ox.ac.uk>
2016-01-12 11:08:58 +0000

    2.8

--- grails-app/controllers/geldb/DispatchRecordController.groovy
+++ grails-app/controllers/geldb/DispatchRecordController.groovy
@@ -422,7 +422,18 @@ class DispatchRecordController {
 //                }
 //
 //                def laboratoryRemainingVolumeBanked  = { domain, value ->
-//                    return "0"
+//                    if (domain?.identifiedSample?.aliquot?.specimen?.toString()?.startsWith('[Fluid Specimen-')) {
+//                        return "0"
+//                    }else{
+//                        def elution = DNA_Extract?.findById(domain?.identifiedSample?.id)?.sapphireIdentifier
+//                        if (elution){
+//                            elution = elution?.toString() + '_Remaining'
+//                            def remainingVolume = DNA_Extract?.findBySapphireIdentifier(elution)?.dNAAmount
+//                            if (remainingVolume){
+//                                return remainingVolume
+//                            }
+//                        }
+//                    }
 //                }
 //
 //                Map formatters = ["identifiedSample.aliquot.specimen.participant.studySubject.studySubjectIdentifier" : gelId,'clinicID':clinicID,"identifiedSample.aliquot.specimen.fFPE_Tissue_Report.tumourStatus":tumourType,
