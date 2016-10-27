a3358d06162ca819f4a89fffe53a44e9a354d357
Nasullah Alham <nasullah.khalidalham@ndcls.ox.ac.uk>
2016-10-27 14:36:11 +0100

    Added 'created on' to the export option

--- grails-app/services/geldb/Aliquot/ExportListOfMaterialSuppliedService.groovy
+++ grails-app/services/geldb/Aliquot/ExportListOfMaterialSuppliedService.groovy
@@ -39,25 +39,30 @@ class ExportListOfMaterialSuppliedService {
             else return ''
         }
 
+        def createdOnFormat = { domain, value ->
+                return value?.format('yyyy-MM-dd')
+        }
+
         Map formatters = ["specimen.participant.studySubject.studySubjectIdentifier": gelId, "specimen.anatomicalSite": anatomicalSite, "exhausted":exhausted,
-                          "gelSuitabilityReport.suitableForGel":suitableForGel]
+                          "gelSuitabilityReport.suitableForGel":suitableForGel, "createdOn":createdOnFormat]
         return formatters
     }
 
     def getFields(){
         List fields = ["specimen.participant.studySubject.studySubjectIdentifier", "specimen.anatomicalSite", "aliquotType", "blockNumber", "aliquotRanking",
-                       "aliquotVolumeMass", "unit", "exhausted", "gelSuitabilityReport.suitableForGel"]
+                       "aliquotVolumeMass", "unit", "exhausted", "createdOn", "gelSuitabilityReport.suitableForGel"]
         return fields
     }
 
     def getLabels(){
         Map labels = ["specimen.participant.studySubject.studySubjectIdentifier": "GEL Study ID", "specimen.anatomicalSite": "Anatomical Site", "aliquotType": "Aliquot Type", "blockNumber": "Block Number",
-                      "aliquotRanking": "Aliquot Ranking", "aliquotVolumeMass": "Mass", "unit": "Unit","exhausted":"Exhausted", "gelSuitabilityReport.suitableForGel":"Suitable for GEL"]
+                      "aliquotRanking": "Aliquot Ranking", "aliquotVolumeMass": "Mass", "unit": "Unit","exhausted":"Exhausted", "gelSuitabilityReport.suitableForGel":"Suitable for GEL",
+                      "createdOn":"Created on"]
         return labels
     }
 
     def getParameters(){
-        Map parameters = [title: "Exported Material Supplied", "column.widths": [0.2, 0.15, 0.25, 0.15, 0.15, 0.1, 0.1, 0.1, 0.2]]
+        Map parameters = [title: "Exported Material Supplied", "column.widths": [0.2, 0.15, 0.25, 0.15, 0.15, 0.1, 0.1, 0.1, 0.1, 0.2]]
         return parameters
     }
 }
