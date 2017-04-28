ee3cc0ff189d489b3468b641fffaffe522f5e195
Nasullah Alham <nasullah.khalidalham@ndcls.ox.ac.uk>
2017-04-28 10:54:13 +0100

    Added data export functions

--- grails-app/controllers/geldb/FluidSpecimenController.groovy
+++ grails-app/controllers/geldb/FluidSpecimenController.groovy
@@ -65,6 +65,175 @@ class FluidSpecimenController {
         [participantList: results.sort {it.studySubject.studySubjectIdentifier.findResult {it?.size() ? it : null}}]
     }
 
+    @Secured(['ROLE_ADMIN'])
+    def exportSpecimen(){
+        if(params?.format && params.format != "html"){
+            response.contentType = grailsApplication.config.grails.mime.types[params.format]
+            response.setHeader("Content-disposition", "attachment; filename= exportSpecimen.${params.extension}")
+
+            List fields = ["id", "participant.studySubject.studySubjectIdentifier", "participant.studySubject.study", "participant.studySubject.consentFormNumber",
+                           "participant.studySubject.consentStatus","participant.studySubject.recruitmentDate","participant.studySubject.recruitedBy",
+                           "participant.studySubject.consentFormVersion", "participant.familyName", "participant.givenName", "participant.dateOfBirth",
+                           "participant.gender", "participant.nHSNumber", "participant.hospitalNumber", "participant.diagnosis", "participant.centre",
+                           "participant.solidSpecimenExpected", "primaryContainer", "fluidSpecimenVolume", "volumeUnit", "fluidSampleType", "timePoint",
+                           "anatomicalSite", "collectionMethod", "methodOfTransportToPathologist", "surgeon", "pathologist", "specimenWeight", "massUnit",
+                           "histologyNumber", "noFFSampleExpected", "numberOfBiopsies", "exhausted", "notes", "collectionDate", "collectionTime",
+                           "collectionLocation", "collectedBy", "reference", "barcode", "fFPE_Tissue_Report.reportDate", "fFPE_Tissue_Report.reportStaff"
+                           , "fFPE_Tissue_Report.comments", "fFPE_Tissue_Report.sampleType", "fFPE_Tissue_Report.stage", "fFPE_Tissue_Report.stagingSystem"
+                           , "fFPE_Tissue_Report.tumourStatus", "fFPE_Tissue_Report.tumourType", "fFPE_Tissue_Report.cold_ischaemia", "fFPE_Tissue_Report.warm_ischaemia"
+                           , "fFPE_Tissue_Report.cellPathReport", "fFPE_Tissue_Report.tissueWorksheet", "fFPE_Tissue_Report.cellPathMacroscopicReport",
+                           "fFPE_Tissue_Report.cellPathMicroscopicReport", "fFPE_Tissue_Report.cellPathSummaryReport", "fFPE_Tissue_Report.cellPathSupplementaryReport",
+                           "fFPE_Tissue_Report.snomed"]
+
+
+            Map labels = ["primaryContainer":"specimen.primaryContainer", "fluidSpecimenVolume":"specimen.fluidSpecimenVolume", "volumeUnit":"specimen.volumeUnit",
+                          "fluidSampleType":"specimen.fluidSampleType","timePoint":"specimen.timePoint","anatomicalSite":"specimen.anatomicalSite",
+                          "collectionMethod":"specimen.collectionMethod", "methodOfTransportToPathologist":"specimen.methodOfTransportToPathologist",
+                          "surgeon":"specimen.surgeon", "pathologist":"specimen.pathologist", "specimenWeight":"specimen.specimenWeight", "massUnit":"specimen.massUnit",
+                          "histologyNumber":"specimen.histologyNumber", "noFFSampleExpected":"specimen.noFFSampleExpected", "numberOfBiopsies":"specimen.numberOfBiopsies",
+                          "exhausted":"specimen.exhausted", "notes":"specimen.notes","collectionDate":"specimen.collectionDate", "collectionTime":"specimen.collectionTime",
+                          "collectionLocation":"specimen.collectionLocation", "collectedBy":"specimen.collectionLocation", "reference":"specimen.reference",
+                          "barcode":"specimen.barcode", "id":"specimen.id"]
+
+            def specimenList = Specimen.list()
+            // Formatter closure
+            def gelID = { domain, value ->
+                return value.toString().replace('[','').replace(']','').replace('null','').replace(',','').trim()
+            }
+
+            def clean = { domain, value ->
+                return value.toString().replace('[','').replace(']','').trim()
+            }
+
+            Map formatters = ["participant.studySubject.studySubjectIdentifier": gelID, "fFPE_Tissue_Report.reportDate": clean, "fFPE_Tissue_Report.reportStaff": clean
+                              , "fFPE_Tissue_Report.comments": clean, "fFPE_Tissue_Report.sampleType": clean, "fFPE_Tissue_Report.stage": clean, "fFPE_Tissue_Report.stagingSystem": clean
+                              , "fFPE_Tissue_Report.tumourStatus": clean, "fFPE_Tissue_Report.tumourType": clean, "fFPE_Tissue_Report.cold_ischaemia": clean, "fFPE_Tissue_Report.warm_ischaemia": clean
+                              , "fFPE_Tissue_Report.cellPathReport": clean, "fFPE_Tissue_Report.tissueWorksheet": clean, "fFPE_Tissue_Report.cellPathMacroscopicReport": clean,
+                              "fFPE_Tissue_Report.cellPathMicroscopicReport": clean, "fFPE_Tissue_Report.cellPathSummaryReport": clean, "fFPE_Tissue_Report.cellPathSupplementaryReport": clean,
+                              "fFPE_Tissue_Report.snomed": clean, "participant.studySubject.study": clean, "participant.studySubject.consentFormNumber": clean,
+                              "participant.studySubject.consentStatus": clean,"participant.studySubject.recruitmentDate": clean,"participant.studySubject.recruitedBy": clean,
+                              "participant.studySubject.consentFormVersion": clean,]
+
+            Map parameters = [title: "Participants", "column.widths": [0.2, 0.2, 0.2, 0.1]]
+            exportService.export(params.format, response.outputStream, specimenList, fields, labels, formatters, parameters )
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def exportAliquot(){
+        if(params?.format && params.format != "html"){
+            response.contentType = grailsApplication.config.grails.mime.types[params.format]
+            response.setHeader("Content-disposition", "attachment; filename= exportAliquot.${params.extension}")
+
+            List fields = ["id", "specimen.id", "exhausted", "notes", "blockNumber", "aliquotType", "barcode", "position","aliquotVolumeMass",
+                           "unit", "sapphireIdentifier", "aliquotRanking", "createdOn", "frozenBy", "createdTime", "gelSuitabilityReport.reportDate"
+                           , "gelSuitabilityReport.reportStaff", "gelSuitabilityReport.comments", "gelSuitabilityReport.handE_pathreview",
+                           "gelSuitabilityReport.slideMarkedBy", "gelSuitabilityReport.sideMarkedDate"
+                           , "gelSuitabilityReport.percentageTumourContent", "gelSuitabilityReport.tumourContentVerifiedBy",
+                           "gelSuitabilityReport.tumourContentVerificationOther", "gelSuitabilityReport.microdissection", "gelSuitabilityReport.microdissectionDetails"
+                           , "gelSuitabilityReport.cellularity", "gelSuitabilityReport.percentageNecrosis", "gelSuitabilityReport.dysplasticNonInvasiveElements"
+                           , "gelSuitabilityReport.dysplasticNonInvasiveNotes", "gelSuitabilityReport.pathologistComments"
+                           , "gelSuitabilityReport.suitableForGel", "gelSuitabilityReport.failureReason", "gelSuitabilityReport.slideScannedOn", "gelSuitabilityReport.slideScannedBy",
+                           "gelSuitabilityReport.handEcarriedOutOn", "gelSuitabilityReport.handEcarriedOutBy", "fixationReport.reportDate", "fixationReport.reportStaff"
+                           , "fixationReport.comments", "fixationReport.fixationTypeAliquot", "fixationReport.processingScheduleAliquot", "fixationReport.fixationStartDateAliquot",
+                           "fixationReport.fixationStartTimeAliquot", "fixationReport.fixationEndDateAliquot", "fixationReport.fixationEndTimeAliquot", "fixationReport.fixationPeriodAliquot",
+                           "fixationReport.fixationTimeUnknownAliquot", "fixationReport.fixationCommentsAliquot" , "fixationReport.tissueProcessorUsed", "fixationReport.timeInProcessor",
+                           "sampleTrackingEvent", "dispatched"]
+
+
+            Map labels = ["exhausted":"aliquot.exhausted", "notes":"aliquot.notes", "blockNumber":"aliquot.blockNumber", "aliquotType":"aliquot.aliquotType",
+                          "barcode":"aliquot.barcode", "position":"aliquot.position","aliquotVolumeMass":"aliquot.aliquotVolumeMass", "id":"aliquot.id",
+                          "unit":"aliquot.unit", "sapphireIdentifier":"aliquot.sapphireIdentifier", "aliquotRanking":"aliquot.aliquotRanking",
+                          "createdOn":"aliquot.createdOn", "frozenBy":"aliquot.frozenBy", "createdTime":"aliquot.createdTime",
+                          "fixationReport.fixationTypeAliquot":"fixationReport.fixationType", "fixationReport.processingScheduleAliquot":"fixationReport.processingSchedule",
+                          "fixationReport.fixationStartDateAliquot":"fixationReport.fixationStartDate", "fixationReport.fixationStartTimeAliquot":"fixationReport.fixationStartTime",
+                          "fixationReport.fixationEndDateAliquot":"fixationReport.fixationEndDate", "fixationReport.fixationEndTimeAliquot":"fixationReport.fixationEndTime",
+                          "fixationReport.fixationPeriodAliquot":"fixationReport.fixationPeriod", "fixationReport.fixationTimeUnknownAliquot":"fixationReport.fixationTimeUnknown",
+                          "fixationReport.fixationCommentsAliquot":"fixationReport.fixationComments"]
+
+            def aliquotList = Aliquot.list()
+            // Formatter closure
+            def gelID = { domain, value ->
+                return value.toString().replace('[','').replace(']','').replace('null','').replace(',','').trim()
+            }
+
+            def clean = { domain, value ->
+                return value.toString().replace('[','').replace(']','').trim()
+            }
+
+            def dispatched = { domain, value ->
+                def dispatchItem = DispatchItem.findByIdentifiedSample(IdentifiedSample.findById(domain.id))
+                if (dispatchItem){
+                    return dispatchItem.dispatchedBox.dispatchRecord.consignmentNumber
+                }else {
+                    return  ""
+                }
+            }
+
+            Map formatters = ["gelSuitabilityReport.reportDate":clean
+                              , "gelSuitabilityReport.reportStaff":clean, "gelSuitabilityReport.comments":clean, "gelSuitabilityReport.handE_pathreview":clean,
+                              "gelSuitabilityReport.slideMarkedBy":clean, "gelSuitabilityReport.sideMarkedDate":clean
+                              , "gelSuitabilityReport.percentageTumourContent":clean, "gelSuitabilityReport.tumourContentVerifiedBy":clean,
+                              "gelSuitabilityReport.tumourContentVerificationOther":clean, "gelSuitabilityReport.microdissection":clean, "gelSuitabilityReport.microdissectionDetails":clean
+                              , "gelSuitabilityReport.cellularity":clean, "gelSuitabilityReport.percentageNecrosis":clean, "gelSuitabilityReport.dysplasticNonInvasiveElements":clean
+                              , "gelSuitabilityReport.dysplasticNonInvasiveNotes":clean, "gelSuitabilityReport.pathologistComments":clean
+                              , "gelSuitabilityReport.suitableForGel":clean, "gelSuitabilityReport.failureReason":clean, "gelSuitabilityReport.slideScannedOn":clean, "gelSuitabilityReport.slideScannedBy":clean,
+                              "gelSuitabilityReport.handEcarriedOutOn":clean, "gelSuitabilityReport.handEcarriedOutBy":clean,
+                              "fixationReport.fixationTypeAliquot":clean, "fixationReport.processingScheduleAliquot":clean,
+                              "fixationReport.fixationStartDateAliquot":clean, "fixationReport.fixationStartTimeAliquot":clean,
+                              "fixationReport.fixationEndDateAliquot":clean, "fixationReport.fixationEndTimeAliquot":clean,
+                              "fixationReport.fixationPeriodAliquot":clean, "fixationReport.fixationTimeUnknownAliquot":clean,
+                              "fixationReport.fixationCommentsAliquot":clean, "fixationReport.tissueProcessorUsed":clean, "fixationReport.timeInProcessor":clean,
+                              "fixationReport.reportDate":clean, "fixationReport.reportStaff":clean, "fixationReport.comments":clean, "dispatched":dispatched,
+                              "sampleTrackingEvent":clean]
+
+            Map parameters = [title: "Participants", "column.widths": [0.2, 0.2, 0.2, 0.1]]
+            exportService.export(params.format, response.outputStream, aliquotList, fields, labels, formatters, parameters )
+        }
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    def exportDNA(){
+        if(params?.format && params.format != "html"){
+            response.contentType = grailsApplication.config.grails.mime.types[params.format]
+            response.setHeader("Content-disposition", "attachment; filename= exportDNA.${params.extension}")
+
+            List fields = ["id", "aliquot.id", "exhausted", "passFail", "passFailReason", "notes", "barcode", "position",
+                           "dNAConcentrationNanodrop", "dNAConcentrationQubit", "extractionDate", "extractedBy", "geLSampleIdentifier",
+                           "sapphireIdentifier", "dNAAmount", "delatQC", "a260A280", "a260A230", "experimentName", "extractionKit",
+                           "extractionType", "rin", "checked", "dispatched"]
+
+
+            Map labels = ["id": "DNA_Extract.id", "exhausted":"DNA_Extract.exhausted", "passFail":"DNA_Extract.passFail", "passFailReason":"DNA_Extract.passFailReason",
+                          "notes":"DNA_Extract.notes", "barcode": "DNA_Extract.barcode", "position":"DNA_Extract.position",
+                          "dNAConcentrationNanodrop":"DNA_Extract.dNAConcentrationNanodrop", "dNAConcentrationQubit":"DNA_Extract.dNAConcentrationQubit",
+                          "extractionDate":"DNA_Extract.extractionDate", "extractedBy":"DNA_Extract.extractedBy", "geLSampleIdentifier":"DNA_Extract.geLSampleIdentifier",
+                          "sapphireIdentifier":"DNA_Extract.sapphireIdentifier", "dNAAmount":"DNA_Extract.dNAAmount", "delatQC":"DNA_Extract.deltaQC", "a260A280":"DNA_Extract.a260A280",
+                          "a260A230":"DNA_Extract.a260A230", "experimentName": "DNA_Extract.experimentName", "extractionKit":"DNA_Extract.extractionKit",
+                          "extractionType":"DNA_Extract.extractionType", "rin":"DNA_Extract.rin", "checked":"DNA_Extract.checked"]
+
+            def dnaList = DNA_Extract.list()
+
+            def clean = { domain, value ->
+                return value.toString().replace('[','').replace(']','').trim()
+            }
+
+            def dispatched = { domain, value ->
+                def dispatchItem = DispatchItem.findByIdentifiedSample(IdentifiedSample.findById(domain.id))
+                if (dispatchItem){
+                    return dispatchItem.dispatchedBox.dispatchRecord.consignmentNumber
+                }else {
+                    return  ""
+                }
+            }
+
+            Map formatters = ["aliquot.id":clean, "dispatched":dispatched]
+
+            Map parameters = [title: "Participants", "column.widths": [0.2, 0.2, 0.2, 0.1]]
+            exportService.export(params.format, response.outputStream, dnaList, fields, labels, formatters, parameters )
+        }
+    }
+
     def awaitingBlood(){
         def results = Participant.list()
         results = results.findAll {participantInstance -> !FluidSpecimen.findByParticipant(participantInstance)}
